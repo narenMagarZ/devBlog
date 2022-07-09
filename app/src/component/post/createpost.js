487e9014b4d0250cfbfad502c4ce56752e287859
narenMagarZ <narenmagarz98@gmail.com>
2022-07-10 00:09:16 +0545

    enable the add tag method

--- app/src/component/post/createpost.js
+++ app/src/component/post/createpost.js
@@ -1,14 +1,14 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './post.css'
 
 export default function CreatePost(){
     let isImgAlreadyPickedUp = useRef(false)
-    function PickCoverImage(ev){
+    function PickCoverImage(){
         this.removeEventListener('change',PickCoverImage)
         const coverImgContent = URL.createObjectURL(this.files[0])
         const coverImg = document.getElementById('cover-img')
         coverImg.src = coverImgContent
-        coverImg.hidden = false
+        coverImg.hidden = coverImg.hidden ? false : false
         const addCoverImgBtn = document.getElementById('cover-img-picker-btn')
         const configureBtnWrapper = document.getElementById('cover-img-configure-wrapper')
         if(!isImgAlreadyPickedUp.current){
@@ -18,7 +18,7 @@ export default function CreatePost(){
         } else if(isImgAlreadyPickedUp.current){
         }
     }
-    function EmitInputPicker(e){
+    function EmitInputPicker(){
         const coverImgPicker = document.getElementById('cover-img-picker')
         coverImgPicker.addEventListener('change',PickCoverImage)
         coverImgPicker.click()
@@ -49,6 +49,7 @@ export default function CreatePost(){
                     </div>
                 </div>
                 <BlogTitleWrapper/>
+                <Tag/>
             </div>
         </div>
     )
@@ -56,36 +57,88 @@ export default function CreatePost(){
 
 function BlogTitleWrapper(){
     const title = useRef('')
-    const isAllTitleContentSelected = useRef(false)
     function ReadInput(e){
-        console.log(e.key)
-        
+        const titleContent = document.getElementById('title-field')
         if(e.key.length === 1){
             title.current += e.key
         }
         else if(e.key === 'Backspace'){
-            title.current = title.current.substring(0,title.current.length - 1)
+            setTimeout(()=>{
+                if(titleContent.innerText.length === 1 ){
+                    title.current = ''
+                    if(title.current.length === 0){
+                        const titlePlaceholder = document.getElementById('title-placeholder')
+                        titlePlaceholder.innerHTML = 'New post title here...'
+                    }
+                }
+            })
+            if(title.current.length > 0)
+                title.current = title.current.substring(0,title.current.length - 1)
         }
-        if(isAllTitleContentSelected.current){
-            if(e.key === 'Backspace') title.current = ''
-        }
-        console.log(title)
         if(title.current.length > 0){
             const titlePlaceholder = document.getElementById('title-placeholder')
             titlePlaceholder.innerHTML = ''
-        } else if(title.current.length === 0) {
-            const titlePlaceholder = document.getElementById('title-placeholder')
-            titlePlaceholder.innerHTML = 'New post title here...'
         }
     }
-    function ReadSelectContent(){
-        isAllTitleContentSelected.current = true
-    }
+
     return (
         <div id='blog-title-wrapper'>
             <span id='title-placeholder'>New post title here...</span>
-            <div onSelect={ReadSelectContent} onKeyDown={ReadInput}  contentEditable='true' id='title-field'>
+            <div onKeyDown={ReadInput}  contentEditable='true' id='title-field'>
             </div>
+        </div>
+    )
+}
+
+function Tag(){
+    const [tags,setTag] = useState([])
+    const inputText = useRef('')
+    const prevInputLen = useRef(0)
+    const lastTagPos = useRef(0)
+    const lastTagLen = useRef(0)
+    function AddTag({target}){
+        console.log(target.value)
+        const inputTag = target.value
+        const isLastValueSpace = inputTag[inputTag.length - 1]
+        if(isLastValueSpace === ' '){
+            // check if first letter is space
+            if(inputText.current.length === 0 && tags.length === 0){
+                target.value = ''
+            }
+            else {
+                // then add tags
+                inputText.current = inputTag.substring(lastTagPos.current,inputTag.length - 1)
+                if(tags.length <5){
+                    setTag((tag)=>[...tag,inputText.current])
+                    lastTagPos.current = inputTag.length
+                }   
+            }
+        } else {
+            inputText.current += inputTag
+        }
+
+        if(inputTag.length === 0){
+            setTag(()=>[])
+            inputText.current = ''
+            lastTagPos.current = 0
+        }
+        if(lastTagPos.current - 1 === inputTag.length && inputTag.length < prevInputLen.current){
+            console.log(tags.length,'this is tags.length')
+            const lastTag = tags[tags.length - 1]
+            console.log(lastTag,'this is lasttag')
+            console.log(lastTag)
+        }
+        prevInputLen.current = inputTag.length
+        console.log(lastTagPos.current,inputTag.length)
+
+    }
+    useEffect(()=>{
+        console.log(tags)
+    },[tags])
+    return(
+        <div id='tag-wrapper'>
+            <div id='tag-field-wrapper'></div>
+            <input onChange={AddTag} placeholder='Add up to 5 tags...' id='tag-field' />
         </div>
     )
 }
