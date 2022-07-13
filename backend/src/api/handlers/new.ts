716126f480a40b986e67b1b5b8eefdb65594fb50
narenMagarZ <narenmagarz98@gmail.com>
2022-07-13 22:48:26 +0545

    changin the res content for dummy blog

--- backend/src/api/handlers/new.ts
+++ backend/src/api/handlers/new.ts
@@ -35,19 +35,8 @@ export default function NewPost(req:DevBlogType.Request,res:Response,next:NextFu
                 'path' : uploadFileInfo['path'] ?? ''
             }
             console.log(postContent)
-            const testcontent = {
-                'title' : 'Simple Http server on nodejs ',
-                'coverimg' : 'https://res.cloudinary.com/practicaldev/image/fetch/s--_QMQU86---/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/6dnng3pre04xxdebia1g.png',
-                'links' : ['www.nodejs.org','www.npmpackage.com'],
-                'content' : `
-                I will show you how to set up a Rest API with Node and Express. I hope you can learn something from this tutorial and I can learn something with your feedback.
-                REST, or REpresentational State Transfer, is an architectural style for providing standards between computer systems on the web,
-                making it easier for systems to communicate with each other. REST-compliant systems, often called RESTful systems, 
-                are characterized by how they are stateless and separate the concerns of client and server.
-                We will go into what these terms mean and why they are beneficial characteristics for services on the Web.
-                `
-            }
-            res.json(testcontent)
+
+            res.json('done')
         }else {
 
         }
