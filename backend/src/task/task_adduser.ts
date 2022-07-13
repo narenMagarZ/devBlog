19157d43b187ced701cfbde18c9dd62b788f6344
narenMagarZ <narenmagarz98@gmail.com>
2022-07-13 13:51:13 +0545

    saving the user to db on login

--- backend/src/task/task_adduser.ts
+++ backend/src/task/task_adduser.ts
@@ -4,7 +4,10 @@ import {getRedisConnection} from '../db/redis/redis'
 
 export async function AddUserToDB(user:DevBlogType.user):Promise<void>{
     try{
-        await db.getUser().insertMany([user])
+        console.log(user)
+        const userModel = db.getUser()
+        const thisUser = new userModel(user)
+        await thisUser.save()
         await getRedisConnection()?.hset('user',user.email,user.uid)
         Logger.success('Success to write the user to db')
     }
