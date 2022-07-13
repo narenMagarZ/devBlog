5c88819b01771b662dca5c3dd7fb8312291a23ac
narenMagarZ <narenmagarz98@gmail.com>
2022-07-13 12:56:26 +0545

    make api call to the endpoint / from homepage

--- app/src/component/home/home.js
+++ app/src/component/home/home.js
@@ -1,7 +1,14 @@
 import './home.css'
-
+import { baseApi } from '../../utils/baseurl'
+import { useEffect } from 'react'
 export default function Home(){
-
+    useEffect(()=>{
+        baseApi.get('/').then(res=>{
+            console.log(res)
+        }).catch(err=>{
+            console.error(err)
+        })
+    },[])
     return(
         <div className='home-wrapper'>
             <div>
