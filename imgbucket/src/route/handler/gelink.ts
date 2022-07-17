06650835d709ad8fcafb94222e8312e02332b76a
narenMagarZ <narenmagarz98@gmail.com>
2022-07-17 17:47:12 +0545

    removed the console.log

--- imgbucket/src/route/handler/gelink.ts
+++ imgbucket/src/route/handler/gelink.ts
@@ -13,11 +13,9 @@ export default function getBucketKey(req:Request,res:Response){
         const {error,value} = reqSchema.validate(req.body)
         if(!error){
             const {fileName,fileSize,fileType} = value
-            console.log(fileName,fileSize,fileType)
             const baseStorage = resolve(__dirname,'../','../','../','./uploads') as string
             const uploadFileName = uuidv4().split('-').join('') + '.' + fileName + '.' +  fileType.split('/')[1] as string
             const generatedLink = baseStorage + '/' + uploadFileName
-            console.log(uploadFileName,baseStorage,generatedLink)
             res.json({
                 link:generatedLink,
                 status:200
